fb30d5f4021af8da7866f3546171b081bf90d28b
Yuhao Zhong <Hao@Yuhaos-MacBook-Pro.local>
2020-08-25 15:15:12 +0200

    Setup Layout

---
+++ app/src/main/java/net/atomday/timefighter/MainActivity.kt
@@ -0,0 +1,13 @@
+package net.atomday.timefighter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
